65260810556867a33b88d8ddece4fa6cfe90f5ea
ruaríø <ruario@vivaldi.com>
2024-02-28 17:37:54 +0100

    Update Vivaldi to 6.5.3206.63

--- vivaldi.sh
+++ vivaldi.sh
@@ -2,9 +2,9 @@
 
 case $(uname -m) in
   x86_64)
-    FFMPEG_SUM=e1cc9bedad03e1c47cb91fd92fd1a59ce8ff9265133fe6d6e344c7c92a7ade9c
-    if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_SUM/libffmpeg.so" ]; then
-      export ZYPAK_LD_PRELOAD="$ZYPAK_LD_PRELOAD${ZYPAK_LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_SUM/libffmpeg.so"
+    FFMPEG_VERSION=114023
+    if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
+      export ZYPAK_LD_PRELOAD="$ZYPAK_LD_PRELOAD${ZYPAK_LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
     else
       echo "'Proprietary media' support is not installed. Attempting to fix this for the next restart." >&2
       nohup "/app/vivaldi/update-ffmpeg" --user > /dev/null 2>&1 &
