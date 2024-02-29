3f99ff6af6cd689eb8c16dc992355e705bbc62c2
ruario <github@ro.mm.st>
2024-02-29 14:09:00 +0100

    Provide a way for arm64 users to supply their own h264/acc support (#33)

--- vivaldi.sh
+++ vivaldi.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/bash
 
+FFMPEG_VERSION=114023
+if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
+  export ZYPAK_LD_PRELOAD="$ZYPAK_LD_PRELOAD${ZYPAK_LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
+fi
+
 case $(uname -m) in
   x86_64)
-    FFMPEG_VERSION=114023
-    VIVALDI_MAJOR_MINOR=6.6
-    if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
-      export ZYPAK_LD_PRELOAD="$ZYPAK_LD_PRELOAD${ZYPAK_LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
-    else
-      if [ -e "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$VIVALDI_MAJOR_MINOR" ]; then
+    if [ ! -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
+      if [ -e "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$FFMPEG_VERSION" ]; then
         echo "'Proprietary media' support is not installed. Attempting to fix this for the next restart." >&2
         nohup "/app/vivaldi/update-ffmpeg" --user > /dev/null 2>&1 &
       else
-        rm -f "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-"*
         echo "'Proprietary media' support is not installed. Attempting to fix this now." >&2
-        timeout 3s "/app/vivaldi/update-ffmpeg" --user
+        timeout 3s "/app/vivaldi/update-ffmpeg" --user > /dev/null 2>&1
         if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
           export ZYPAK_LD_PRELOAD="$ZYPAK_LD_PRELOAD${ZYPAK_LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
         else
           nohup "/app/vivaldi/update-ffmpeg" --user > /dev/null 2>&1 &
         fi
-        touch "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$VIVALDI_MAJOR_MINOR"
+        rm -f "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-"*
+        touch "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$FFMPEG_VERSION"
       fi
     fi
     ;;
