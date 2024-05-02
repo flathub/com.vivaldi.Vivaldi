7a2f372581bb0ad626159cdcdc4ad7816520f652
ruario <github@ro.mm.st>
2024-05-02 13:28:54 +0200

    Update to 6.7.3329.24
    * Update to 6.7.3329.24
    * Make the update-ffmpeg more readable and hide a confusing error

--- vivaldi.sh
+++ vivaldi.sh
@@ -11,15 +11,15 @@ case $(uname -m) in
       if [ -e "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$FFMPEG_VERSION" ]; then
         echo "'Proprietary media' support is not installed. Attempting to fix this for the next restart." >&2
         export VIVALDI_FFMPEG_FUTURE_PATH="$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
-        nohup sh -c "sleep 10; \"/app/vivaldi/update-ffmpeg\" --user" > /dev/null 2>&1 &
+        nohup sh -c "sleep 10; /app/vivaldi/update-ffmpeg --user" > /dev/null 2>&1 &
       else
         echo "'Proprietary media' support is not installed. Attempting to fix this now." >&2
-        timeout 3s "/app/vivaldi/update-ffmpeg" --user
+        timeout 3s /app/vivaldi/update-ffmpeg --user 2> /dev/null
         if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
           export ZYPAK_LD_PRELOAD="$ZYPAK_LD_PRELOAD${ZYPAK_LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
         else
           export VIVALDI_FFMPEG_FUTURE_PATH="$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
-          nohup sh -c "sleep 7; \"/app/vivaldi/update-ffmpeg\" --user" > /dev/null 2>&1 &
+          nohup sh -c "sleep 7; /app/vivaldi/update-ffmpeg --user" > /dev/null 2>&1 &
         fi
         rm -f "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-"*
         touch "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$FFMPEG_VERSION"
