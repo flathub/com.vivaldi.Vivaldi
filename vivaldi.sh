e7ee31945cfd90777487f3ff669ea7e363679990
ruaríø <ruario@vivaldi.com>
2024-09-18 14:54:59 +0200

    Update Vivaldi to 6.9.3447.46

--- vivaldi.sh
+++ vivaldi.sh
@@ -3,32 +3,25 @@
 FFMPEG_VERSION=115541
 if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
   export LD_PRELOAD="$LD_PRELOAD${LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
+else
+  if [ -e "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$FFMPEG_VERSION" ]; then
+    echo "'Proprietary media' support is not installed. Attempting to fix this for the next restart." >&2
+    export VIVALDI_FFMPEG_FUTURE_PATH="$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
+    nohup sh -c "sleep 10; /app/vivaldi/update-ffmpeg --user" > /dev/null 2>&1 &
+  else
+    echo "'Proprietary media' support is not installed. Attempting to fix this now." >&2
+    timeout 3s /app/vivaldi/update-ffmpeg --user 2> /dev/null
+    if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
+      export LD_PRELOAD="$LD_PRELOAD${LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
+    else
+      export VIVALDI_FFMPEG_FUTURE_PATH="$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
+      nohup sh -c "sleep 7; /app/vivaldi/update-ffmpeg --user" > /dev/null 2>&1 &
+    fi
+    rm -f "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-"*
+    touch "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$FFMPEG_VERSION"
+  fi
 fi
 
-case $(uname -m) in
-  x86_64)
-    if [ ! -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
-      if [ -e "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$FFMPEG_VERSION" ]; then
-        echo "'Proprietary media' support is not installed. Attempting to fix this for the next restart." >&2
-        export VIVALDI_FFMPEG_FUTURE_PATH="$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
-        nohup sh -c "sleep 10; /app/vivaldi/update-ffmpeg --user" > /dev/null 2>&1 &
-      else
-        echo "'Proprietary media' support is not installed. Attempting to fix this now." >&2
-        timeout 3s /app/vivaldi/update-ffmpeg --user 2> /dev/null
-        if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so" ]; then
-          export LD_PRELOAD="$LD_PRELOAD${LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
-        else
-          export VIVALDI_FFMPEG_FUTURE_PATH="$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$FFMPEG_VERSION/libffmpeg.so"
-          nohup sh -c "sleep 7; /app/vivaldi/update-ffmpeg --user" > /dev/null 2>&1 &
-        fi
-        rm -f "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-"*
-        touch "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$FFMPEG_VERSION"
-      fi
-    fi
-    ;;
-  aarch64)
-    export LIBGL_DRIVERS_PATH=/usr/lib/aarch64-linux-gnu/GL/lib/dri
-    ;;
-esac
+[ "$(uname -m)" = "aarch64" ] && export LIBGL_DRIVERS_PATH=/usr/lib/aarch64-linux-gnu/GL/lib/dri
 
 exec cobalt "$@" --no-default-browser-check
