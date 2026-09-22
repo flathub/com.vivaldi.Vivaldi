35fbd5559aef51bf4ae27972534900d0feaf8dcd
ruario <github@ro.mm.st>
2026-09-22 21:15:21 +0200

    Simplify ffmpeg location script

--- vivaldi.sh
+++ vivaldi.sh
@@ -13,24 +13,8 @@ for FFMPEG_VERSION_CANDIDATE in $FFMPEG_VERSIONS; do
   fi
 done
 if [ "$FFMPEG_FOUND" = NO ]; then
-  if [ -e "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$VIVALDI_VERSION_SHORT" ]; then
-    echo "'Proprietary media' support is not installed. Attempting to fix this for the next restart." >&2
-    export VIVALDI_FFMPEG_FUTURE_PATH="$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$VIVALDI_VERSION_SHORT/libffmpeg.so"
-    nohup sh -c "sleep 10; /app/vivaldi/update-ffmpeg --user" > /dev/null 2>&1 &
-  else
-    rm -f "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-"*
-    echo "'Proprietary media' support is not installed. Attempting to fix this now." >&2
-    timeout 3s /app/vivaldi/update-ffmpeg --user 2> /dev/null
-    if [ -e "$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$VIVALDI_VERSION_SHORT/libffmpeg.so" ]; then
-      export LD_PRELOAD="$LD_PRELOAD${LD_PRELOAD:+:}$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$VIVALDI_VERSION_SHORT/libffmpeg.so"
-    else
-      export VIVALDI_FFMPEG_FUTURE_PATH="$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$VIVALDI_VERSION_SHORT/libffmpeg.so"
-      nohup sh -c "sleep 7; /app/vivaldi/update-ffmpeg --user" > /dev/null 2>&1 &
-    fi
-    rm -f "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-"*
-    mkdir -p "$XDG_DATA_HOME"
-    touch "$XDG_DATA_HOME/vivaldi-update-ffmpeg-checked-$VIVALDI_VERSION_SHORT"
-  fi
+  export VIVALDI_FFMPEG_FUTURE_PATH="$XDG_DATA_HOME/vivaldi-extra-libs/media-codecs-$VIVALDI_VERSION_SHORT/libffmpeg.so"
+  nohup sh -c "sleep 7; /app/vivaldi/update-ffmpeg --user" > /dev/null 2>&1 &
 fi
 
 if [ "$(uname -m)" = "aarch64" ]; then
